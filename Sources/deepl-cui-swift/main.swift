262d70929563bc874b87c8052fc4f5c2ca706f8a
tokorom <tokorom@gmail.com>
2020-12-14 18:40:41 +0900

    Use env values

--- Sources/deepl-cui-swift/main.swift
+++ Sources/deepl-cui-swift/main.swift
@@ -3,6 +3,8 @@ import DeepLCore
 import Foundation
 
 struct Command: ParsableCommand {
+  static let envNameDeepLAuthKey = "DEEPL_AUTH_KEY"
+
   @Argument(help: "Text to be translated. If not specified, use STDIN.")
   var text: String?
 
@@ -16,11 +18,25 @@ struct Command: ParsableCommand {
   @Option(name: .shortAndLong, help: "The language into which the text should be translated")
   var targetLang: String = "EN-US"
 
-  mutating func run() throws {
-    let deepL = DeepL(authKey: "SAMPLE")
+  @Option(
+    name: .long,
+    help: "authKey for DeepL API. If not specified, use the environment variable DEEPL_AUTH_KEY."
+  )
+  var authKey: String?
 
+  mutating func run() throws {
     let group = DispatchGroup()
     group.enter()
+
+    let authKey: String
+
+    if let validAuthKey = self.authKey {
+      authKey = validAuthKey
+    } else if let validAuthKey = ProcessInfo.processInfo.environment[Self.envNameDeepLAuthKey] {
+      authKey = validAuthKey
+    } else {
+      fatalError("\(Self.envNameDeepLAuthKey) is not found.")
+    }
 
     let text: String
 
@@ -30,6 +46,8 @@ struct Command: ParsableCommand {
       let standardInput = FileHandle.standardInput
       text = String(data: standardInput.availableData, encoding: .utf8) ?? ""
     }
+
+    let deepL = DeepL(authKey: authKey)
 
     deepL.translate(text: text, sourceLang: sourceLang, targetLang: targetLang) { result in
       print(result)
